c54975c2b75d2afcc349f4dce36fcfc162cd3cdf
Cissica Gonzalez Martinez <cecemartinez@hotmail.com>
2020-08-02 05:40:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
-  elsif condition
-    return
-  else
-    return
+
 end
 
 # Whatever you say to grandma, she should respond with
